69f5d3c66d917c7b6f59b933063019021845ec08
= <=>
2020-06-17 17:23:57 +0200

    developpement des fichiers

---
+++ view/Accueil_vue.php
@@ -0,0 +1,208 @@
+
+
+ 
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background: url(file/img/slide/slide-1.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animated fadeInDown">Bienvenue sur <span><?=APP_NAME?></span></h2>
+                <p class="animated fadeInUp">Découvrez nos annonces de particuliers et professionnels partout en France. Annonce Gratuite. Pour Les Particuliers. Pour Les Professionnels. Catégories: Immobilier, Auto - Moto, Emploi, Animaux,Services..</p>
+                <a href="" class="btn-get-started animated fadeInUp">Déposer une annonce</a>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 2 -->
+          <div class="carousel-item" style="background: url(file/img/slide/slide-2.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animated fadeInDown"><?=APP_NAME?> <span>Annonces gratuites </span></h2>
+                <p class="animated fadeInUp">Découvrez nos annonces gratuites de particuliers et pros : faites de bonnes affaires d'achat, vente et déposez votre annonce gratuite ...</p>
+                <a href="" class="btn-get-started animated fadeInUp">En Savoir Plus</a>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 3 -->
+          <div class="carousel-item" style="background: url(file/img/slide/slide-3.jpg)">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animated fadeInDown">PME <span> Service 100% gratuit</span></h2>
+                <p class="animated fadeInUp">Publier ou consulter des petites annonces sur <?=APP_NAME?> est 100% gratuit. Aucun paiement ne vous sera jamais demandé, ni avant ni après. C'est le seul site du secteur à audience massive totalement gratuit, y compris pour les professionnels.</p>
+                <a href="" class="btn-get-started animated fadeInUp">Publier une annonce </a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon icofont-rounded-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon icofont-rounded-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Featured Section ======= -->
+    <section id="featured" class="featured">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="icon-box">
+              <i class="icofont-computer"></i>
+              <h3><a href="">Gratuit et Modifiable</a></h3>
+              <p>" Chez nous, la gratuité c'est un état d'esprit. Déposer une petite annonce sur notre site internet, c'est gratuit et vous n'avez aucun frais superflus pour la corriger ou ajouter des photos."</p>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="icon-box">
+              <i class="icofont-image"></i>
+              <h3><a href="">Votre fidélité est récompensée</a></h3>
+              <p>" À chaque dépôt d'annonce gratuite, vous gagnez des points bonus. Avec ces points, vous bénéficiez d'options de visibilité pour vos annonces."</p>
+            </div>
+          </div>
+          <div class="col-lg-4 mt-4 mt-lg-0">
+            <div class="icon-box">
+              <i class="icofont-tasks-alt"></i>
+              <h3><a href="">Votre profil est unique</a></h3>
+              <p>" Vous pouvez personnaliser votre page vendeur. Vous créez un avatar à votre image, vous obtenez des badges permettant de mieux illustrer votre profil (adresse vérifiée)."</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Featured Section -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="file/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <h3>Pourquoi déposer une annonce sur <?=APP_NAME?> ?</h3>
+            <p class="font-italic">
+              <?=APP_NAME?> est un acteur majeur dans le secteur des petites annonces francophones. En annonçant sur ce site, vous offrez une visibilité maximale à vos annonces.
+
+            </p>
+            <ul>
+              <li><i class="icofont-check-circled"></i> Interface rapide et filtrage précis des annonces.</li>
+              <li><i class="icofont-check-circled"></i> Messagerie interne, chat et forum de discussion.</li>
+              <li><i class="icofont-check-circled"></i> <?=APP_NAME?> est équipé de multiples systèmes de lutte anti-spam. Votre adresse email n'est jamais fournie aux utilisateurs, elle est remplacée par une adresse du type sécuriser. Votre numéro de téléphone est affiché sous forme d'image.</li>
+            </ul>
+            <p>
+              Vous pouvez publier autant d'annonces que vous le souhaitez, à condition qu'elles respectent nos conditions d'utilisation et qu'elles ne proposent aucun contenu interdit. Vous pouvez également publier jusqu'à 5 photos par annonce.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Quelques catégories...</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4><a href="">Annonces immobilières</a></h4>
+              <p>Pour acheter ou louer votre bien immobilier, vous êtes à la bonne adresse ! Découvrez les annonces immo de <?=APP_NAME?>, en ce moment des dizaines de milliers d'offres de biens immobiliers sont disponibles à la location ou en vente.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Annonces auto, moto, bateau</a></h4>
+              <p> Des milliers d'annonces de voitures, utilitaires, 2 roues ou véhicules nautiques sont à votre disposition sur <?=APP_NAME?></p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4><a href="">Annonces mon débarras</a></h4>
+              <p>La rubrique mon débarras vous permet d'acheter et de vendre tout objet d'occasion. Vous y trouverez des milliers d'annonces de meubles, de vêtements, d'ordinateurs, ...</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-world"></i></div>
+              <h4><a href="">Annonces location vacances</a></h4>
+              <p>Trouvez votre location de vacances parmi les petites annonces <?=APP_NAME?>. Que vous soyez adepte de vacances à la mer, au ski ou à la campagne, vous trouverez la location saisonnière adaptée à vos envies parmi nos offres de particuliers ou professionnels.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-slideshow"></i></div>
+              <h4><a href="">Annonces animaux</a></h4>
+              <p>Si vous souhaitez adopter un animal de compagnie alors vous êtes sur le site de petites annonces qu'il vous faut. Consultez les milliers d'annonces disponibles et adoptez votre animal.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-arch"></i></div>
+              <h4><a href="">Annonces services</a></h4>
+              <p>Pour faire appel à un prestataire de services qu'il soit particulier ou professionnel, rien de plus simple. Consultez nos annonces de services à la personne, à domicile ou hors domicile et trouvez l'offre correspondant à vos besoins.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Clients</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="owl-carousel clients-carousel">
+          <img src="file/img/clients/client-1.png" alt="">
+          <img src="file/img/clients/client-2.png" alt="">
+          <img src="file/img/clients/client-3.png" alt="">
+          <img src="file/img/clients/client-4.png" alt="">
+          <img src="file/img/clients/client-5.png" alt="">
+          <img src="file/img/clients/client-6.png" alt="">
+          <img src="file/img/clients/client-7.png" alt="">
+          <img src="file/img/clients/client-8.png" alt="">
+        </div>
+
+      </div>
+    </section><!-- End Clients Section -->
+
+  </main><!-- End #main -->
--- view/header.php
+++ view/header.php
@@ -1,6 +1,19 @@
 <meta charset="<?=APP_DEFAULT_CHARSET?>">
 <meta name="viewport" content="width=*, initial-scale=1.0">
-<title>Document</title>
+<title><?=isset($data->meta)?$data->meta['title']:APP_NAME?></title>
+<meta content="" name="descriptison">
 <!-- Favicon -->
 <link href="<?=CONFIG_APP_FAVICON?>" rel="shortcut icon"/>
- 
+ <!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="dist/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="dist/vendor/icofont/icofont.min.css" rel="stylesheet">
+<link href="dist/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="dist/vendor/animate.css/animate.min.css" rel="stylesheet">
+<link href="dist/vendor/owl.carousel/dist/owl.carousel.min.css" rel="stylesheet">
+<link href="dist/vendor/venobox/venobox.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="dist/css/style.css" rel="stylesheet">
