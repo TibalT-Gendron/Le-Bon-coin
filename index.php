4166c6d472b841a672e6790d889950e37953f7bc
= <=>
2020-06-17 17:24:33 +0200

    modification de index.php

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
    require_once('class/Autoloader.class.php');
    AutoLoader::register();
    $router= new AltoRouter();
-
+   $GLOBALS['controller']=new Controller();
    #Creation des routes
+
    $router->map( 'GET', '/', function() {
-    require __DIR__ . '/views/home.php';
+    $GLOBALS['controller']->initPage("Home","accueil");
 	});
+   //var_dump($_SERVER['REQUEST_URI']);die;
+   $match = $router->match();
+   // call closure or throw 404 status
+	if( $match && is_callable( $match['target'] ) ) {
+		call_user_func_array( $match['target'], $match['params'] );
+	} else {
+		$GLOBALS['controller']->initPage("404");
+	}
 ?>
 
