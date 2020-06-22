c94a278ac0e9b4457469339e31a9a68a9faf5514
= <=>
2020-06-22 08:26:10 +0200

    avancement du code

--- dist/js/main.js
+++ dist/js/main.js
@@ -129,22 +129,8 @@
   });
 
   // Clients carousel (uses the Owl Carousel library)
-  $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 2
-      },
-      768: {
-        items: 4
-      },
-      900: {
-        items: 6
-      }
-    }
-  });
+  
+  $(".owl-carousel").owlCarousel();
 
   // jQuery counterUp
   $('[data-toggle="counter-up"]').counterUp({
