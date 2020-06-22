f5b1d858004098c825454b67b5f38ece2e6a79a0
= <=>
2020-06-22 08:24:15 +0200

    avancement du code

--- config/config.php
+++ config/config.php
@@ -1,12 +1,13 @@
 <?php
 define("APP_NAME","Leboncoin");
-define("APP_DFAULT_DESCRIPTION","Leboncoin");
+define("APP_DFAULT_DESCRIPTION","Leboncoin est une plateforme de publication d'annonce a nantes");
 define("APP_CONTACT_NUMBER","Leboncoin");
 define("APP_CONTACT_EMAIL","Leboncoin");
 define("APP_LOCATION",$_SERVER['REQUEST_URI']);
 define("APP_BASE_URL",$_SERVER['HTTP_HOST']);
 define("APP_LOGO","");
-define("CONFIG_APP_FAVICON","");
+define("CONFIG_APP_FAVICON","/file/img/favicon.png");
+define("CONFIG_APP_LOGO","/file/img/logo.png");
 define("APP_DEFAULT_LANGUE",'FR-fr');
 define("APP_DEFAULT_CHARSET",'UTF-8');
 define("APP_TIME_FUSEAU","Africa/Porto-novo");
