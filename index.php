d498227b93b26e919d93c19d68b68b0c5651a413
= <=>
2020-06-25 02:53:02 +0200

    fin de index.php

--- index.php
+++ index.php
@@ -6,9 +6,30 @@
    $GLOBALS['controller']=new Controller();
    $GLOBALS['database']=new Database();
    #Creation des routes
-
-   $router->map( 'GET', '/', function() {
+   //accueil mapping
+   $router->map( 'GET|POST', '/', function() {
     $GLOBALS['controller']->initPage("Home","accueil");
+	});
+   //login mapping
+   $router->map( 'GET|POST', '/compte', function() {
+    $GLOBALS['controller']->initPage("Login","display");
+	});
+   //profile mapping
+   $router->map( 'GET|POST', '/profile', function() {
+    $GLOBALS['controller']->initPage("Profile","display");
+	});
+   $router->map( 'GET|POST', '/profile-[*:typeof]', function($typeof) {
+   	$GLOBALS["typeof"]=$typeof;
+    $GLOBALS['controller']->initPage("Profile","display");
+	});
+
+   //deconnexion
+   $router->map( 'GET', '/deconnexion', function() {
+    $GLOBALS['controller']->initPage("Login","logout");
+	});
+   //inscription mapping
+   $router->map( 'GET|POST', '/inscription', function() {
+    $GLOBALS['controller']->initPage("Login","registerpage");
 	});
    //var_dump($_SERVER['REQUEST_URI']);die;
    $match = $router->match();
@@ -16,7 +37,7 @@
 	if( $match && is_callable( $match['target'] ) ) {
 		call_user_func_array( $match['target'], $match['params'] );
 	} else {
-		$GLOBALS['controller']->initPage("404");
+		$GLOBALS['controller']->initPage("404","notfound");
 	}
 ?>
 
